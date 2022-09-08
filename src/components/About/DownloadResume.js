a6064488561a5a96cbd35340e2c7702a2914e2a6
Santhosh <99713211+San0077@users.noreply.github.com>
2022-09-08 16:17:11 +0530

    Update DownloadResume.js

--- src/components/About/DownloadResume.js
+++ src/components/About/DownloadResume.js
@@ -4,7 +4,7 @@ import DownloadIcon from "../../images/download-icon.png";
 const DownloadResume = () => {
   return (
     <section>
-      <a className="download-btn" href="https://drive.google.com/file/d/1H1o3e7XgyjXqS2TsYR6gp0xZusUmjVj0/view" >
+      <a className="download-btn" href="https://drive.google.com/file/d/1H1o3e7XgyjXqS2TsYR6gp0xZusUmjVj0/view" target="_blank" >
         <img src={DownloadIcon} alt="" className="download-img"></img>Download
         Resume
       </a>
